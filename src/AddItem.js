fc8307121a58bffd8bb569b5e4bd37c1bb92730e
Karl Morand <karljmorand@gmail.com>
2018-09-09 22:23:29 -0400

    update style of add item page and auto-focus on text input

--- src/AddItem.js
+++ src/AddItem.js
@@ -12,12 +12,18 @@ export default class AddItem extends Component {
   state = {
     newItem: ''
   };
+  componentDidMount() {
+    this.textInput.focus();
+  }
   render() {
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding">
         <Text style={styles.title}>Add Item</Text>
         <TextInput
           style={styles.input}
+          ref={input => {
+            this.textInput = input;
+          }}
           onChangeText={newItem => this.setState({ newItem })}
         />
         <View style={styles.buttonRow}>
@@ -42,7 +48,7 @@ export default class AddItem extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
+    backgroundColor: 'white',
     justifyContent: 'center',
     alignItems: 'center'
   },
@@ -59,7 +65,8 @@ const styles = StyleSheet.create({
   },
   buttonStyle: {
     padding: 20,
-    backgroundColor: 'orange'
+    backgroundColor: 'orange',
+    borderRadius: 10
   },
   buttonText: {
     textAlign: 'center',
@@ -72,6 +79,8 @@ const styles = StyleSheet.create({
     width: '80%',
     borderRadius: 10,
     marginVertical: 20,
-    fontSize: 25
+    fontSize: 25,
+    borderColor: 'orange',
+    borderWidth: 2
   }
 });
